70d13b6b177c5f15a26015d320b8081241ad5eac
Jared Heath <jared@piqo.co>
2015-05-03 06:52:01 -1000

    Add configurable cooldown duration; store players by UUID

--- src/net/darkholm/nerfthegap/NerfTheGap.java
+++ src/net/darkholm/nerfthegap/NerfTheGap.java
@@ -12,22 +12,28 @@ import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
+import java.util.UUID;
 
 /**
- * A plugin that adds a cooldown to God apple consumption
+ * A plugin that adds a configurable cooldown to God apple consumption
  *
  * @author Kowagatte
  */
 public class NerfTheGap extends JavaPlugin implements Listener {
 
     /*
-    In Java 7 and later version, new ArrayList<Player>() can simply be written new ArrayList<>() but in order to
+    In Java 7 and later version, new ArrayList<UUID>() can simply be written new ArrayList<>() but in order to
     provide compatibility with Java 6 and earlier versions, this is not done here.
      */
-    private HashSet<String> cooldown = new HashSet<String>();
+    private HashSet<UUID> cooldown = new HashSet<UUID>();
+
+    private int cooldownDuration = 60; // The length of the cooldown, in seconds. 60 by default.
 
     @Override
     public void onEnable() {
+        saveDefaultConfig(); // Imports nodes into the config.yml if empty; otherwise, does not edit the config file
+        reloadConfig(); // Loads the config from its YAML file
+        cooldownDuration = getConfig().getInt("cooldown-seconds");
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -35,7 +41,7 @@ public class NerfTheGap extends JavaPlugin implements Listener {
     public void onGappleUse(PlayerInteractEvent event) {
         if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) &&
                 event.getItem().getType() == Material.GOLDEN_APPLE && event.getItem().getDurability() == 1) {
-            if (this.cooldown.contains(event.getPlayer().getName())) {
+            if (this.cooldown.contains(event.getPlayer().getUniqueId())) {
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(ChatColor.RED + "You cannot eat another God Apple yet.");
             }
@@ -45,15 +51,17 @@ public class NerfTheGap extends JavaPlugin implements Listener {
     @EventHandler
     public void onGappleConsume(PlayerItemConsumeEvent event) {
         if (event.getItem().getType() == Material.GOLDEN_APPLE && event.getItem().getDurability() == 1) {
-            final Player player = event.getPlayer();
-            this.cooldown.add(player.getName());
+            final UUID uuid = event.getPlayer().getUniqueId();
+            this.cooldown.add(uuid);
             Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
                 @Override
                 public void run() {
-                    player.sendMessage(ChatColor.DARK_AQUA + "You can now eat a God Apple again.");
-                    NerfTheGap.this.cooldown.remove(player.getName());
+                    Player player = getServer().getPlayer(uuid);
+                    if (player != null)
+                        player.sendMessage(ChatColor.DARK_AQUA + "You can now eat a God Apple again.");
+                    NerfTheGap.this.cooldown.remove(uuid);
                 }
-            }, 1200L);
+            }, cooldownDuration * 20L);
         }
     }
 }
